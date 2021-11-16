90fbccd34057e8aba795f5ecb0dcd9c6720e83a6
noamriahi <noamriahi10@gmail.com>
2021-11-16 20:59:23 +0200

    to pc

--- Zombies/Assets/Scripts/EnemyAI.cs
+++ Zombies/Assets/Scripts/EnemyAI.cs
@@ -10,6 +10,7 @@ public class EnemyAI : MonoBehaviour
 
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     void Start()
     {
@@ -20,10 +21,35 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if(distanceToTarget <= chaseRange)
+        if(isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget();
         }
+        else if(distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+
+            //navMeshAgent.SetDestination(target.position);
+        }
+    }
+    private void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+    private void AttackTarget()
+    {
+        Debug.Log(name + " has seeked and is destroing " + target.name);
     }
     private void OnDrawGizmosSelected()
     {
